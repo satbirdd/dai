7fc5c550c6c5ec139b2cbb532af76df05e64b1e0
rakeLL <satbirdd@gmail.com>
2014-08-15 16:57:40 +0800

    add

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
   def index
   	@search_conditions = []
+    @url_data = {}
+    @filter_conditions = {}
+
   	@category_id = params[:cat]
   	@brand = params[:brand]
   	@props = params[:prop]
@@ -10,6 +13,16 @@ class SearchController < ApplicationController
   	unless @category_id.blank?
   		@items = @items.joins(product: :category).where("categories.id = ?", @category_id)
   		@search_conditions << Category.find(@category_id).name
+      @url_data[:cat] = @category_id
+
+      category = Category.find(@category_id)
+      category.properties.each do |property|
+        @filter_conditions[property.id] ||= {}
+        @filter_conditions[property.id][:name] = property.name
+        @filter_conditions[property.id][:items] = property.property_items.map do |item|
+          {id: item.id, name: item.name}
+        end
+      end
   	end
 
 	  unless @props.blank?
@@ -24,6 +37,7 @@ class SearchController < ApplicationController
   		prop_pairs = @props.split(';')
   		prop_pairs.each do |pair|
   			key, value = pair.split(':')
+        @url_data[key.to_sym] = value
   			product_properties = product_properties.where(property_id: key, property_item_id: value)
   			@search_conditions << PropertyItem.find(value).name
   		end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,17 @@
 module SearchHelper
+	def search_url_generator(type, *options)
+		case type.to_s
+		when "cat"
+			@url_data[:cat] = options.first
+		when "prop"
+			@url_data[:prop] ||= {}
+			@url_data[:prop][options.first.to_s.to_sym] = options.last
+		end
+
+		props = @url_data[:prop].map do |key, value|
+			"#{key}:#{value}"
+		end.join(';')
+		props = "&prop=" << props
+		"/search?cat=#{@url_data[:cat]}#{props}"
+	end
 end

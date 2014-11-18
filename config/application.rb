6dd490a3e95a9fa516c2eccbe4758adf823faadf
rakeLL <satbirdd@gmail.com>
2014-11-18 13:41:51 +0800

    add config

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,18 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+%w(
+  active_record
+  action_controller
+  action_view
+  action_mailer
+).each do |framework|
+  begin
+    require "#{framework}/railtie"
+  rescue LoadError
+  end
+end
+
+# require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,5 +31,19 @@ module Dai
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.time_zone = 'Beijing'
+
+    # config.autoload_paths += %W(vendor/neil-models/app/models)
+    # config.paths["db/migrate"] << "vendor/neil-models/db/migrate"
+     #    app.config.paths["db/migrate"] << %W(vendor/neil-models/db/migrate)
+     # end    
+    # config.paths["db/migrate"] += %W(vendor/neil-models/db/migrate)
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config','locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = 'zh-CN'
+    config.i18n.enforce_available_locales = true
   end
 end
